a967c47a9d7450f36e3443e1717496ce8c984cb5
Hendri Yana <yhendri568@gmail.com>
2023-11-20 19:09:51 +0700

    first commit

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    public function address() {
+        return view('address');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\AboutusController;
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactusController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/news', [NewsController::class,'index']);
+Route::get('/category', [CategoryController::class,'index']);
+Route::get('/aboutus', [AboutusController::class,'aboutus']);
+Route::get('/contactus', [ContactusController::class,'contactus']);
+Route::get('/address', [AddressController::class,'address']);
